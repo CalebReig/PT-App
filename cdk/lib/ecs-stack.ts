fd768fcb2905017ba99dc811689dcd11b9cc8b6a
CalebReig <reigadacaleb@gmail.com>
2023-03-29 11:44:54 -0700

    added link to backend and dynamodb

--- cdk/lib/ecs-stack.ts
+++ cdk/lib/ecs-stack.ts
@@ -1,11 +1,13 @@
 import { Vpc }  from "aws-cdk-lib/aws-ec2";
 import { 
   Role,
-  ServicePrincipal
+  ServicePrincipal,
+  PolicyStatement
 } from "aws-cdk-lib/aws-iam";
 import { 
   FargateTaskDefinition,
-  ContainerImage 
+  ContainerImage,
+  AwsLogDriver 
 } from "aws-cdk-lib/aws-ecs";
 import { ApplicationLoadBalancedFargateService } from "aws-cdk-lib/aws-ecs-patterns";
 import { 
@@ -15,6 +17,12 @@ import {
 import { DockerImageAsset } from 'aws-cdk-lib/aws-ecr-assets';
 import { Construct } from 'constructs';
 import * as path from 'path';
+import { 
+  CfnService,
+  PrivateDnsNamespace 
+} from 'aws-cdk-lib/aws-servicediscovery';
+import { Table, AttributeType } from 'aws-cdk-lib/aws-dynamodb';
+
 
 export class EcsStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
@@ -24,14 +32,22 @@ export class EcsStack extends Stack {
     const vpc = Vpc.fromLookup(this, "VPC", {
       isDefault: true
     });
+
+    const namespace = new PrivateDnsNamespace(this, 'PTAPPNamespace', {
+      name: 'pt-app.local',
+      vpc: vpc,
+    });
+
     // IAM Role
     const taskIamRole = new Role(this, "AppRole", {
       roleName: "AppRole",
       assumedBy: new ServicePrincipal('ecs-tasks.amazonaws.com'),
     });
+
     const taskDefinition = new FargateTaskDefinition(this, 'Task', {
       taskRole: taskIamRole,
     });
+
     // Docker image Assets
     const frontEndImage = new DockerImageAsset(this, 'FrontEndImage', {
         directory: path.join(__dirname, '../..', 'front-end'),
@@ -39,25 +55,58 @@ export class EcsStack extends Stack {
           buildArgs: false,
         },
       });
+
     const backEndImage = new DockerImageAsset(this, 'BackEndImage', {
         directory: path.join(__dirname, '../..', 'back-end'),
         invalidation: {
           buildArgs: false,
           },
         });
-    // Docker Containers
+
+    // ECS Service discovery
+    const service = new CfnService(this, 'Service', {
+      name: 'my-service',
+      namespaceId: namespace.namespaceId,
+      dnsConfig: {
+        dnsRecords: [
+          {
+            ttl: 60,
+            type: 'A'
+          }
+        ],
+        namespaceId: namespace.namespaceId,
+        routingPolicy: 'MULTIVALUE'
+      },
+      healthCheckCustomConfig: {
+        failureThreshold: 1
+      }
+    });
+
+    const serviceArn = service.attrArn;
+    const serviceUrl = `http://${serviceArn.split('/')[1]}`;
+
+    // Frontend container added to ECS cluster with link to backend
     taskDefinition.addContainer('FrontEndContainer', {
       image: ContainerImage.fromDockerImageAsset(frontEndImage),
       portMappings: [{ containerPort: 3000 }],
       memoryReservationMiB: 50,
       cpu: 1,
+      logging: new AwsLogDriver({ streamPrefix: 'front-end' }),
+      environment: {
+        BACKEND_URL: `${serviceUrl}:8000`,
+      },
     });
-    taskDefinition.addContainer('BackEndContainer', {
+
+    // Backend container added to ECS cluster
+    const backendContainer = taskDefinition.addContainer('BackEndContainer', {
       image: ContainerImage.fromDockerImageAsset(backEndImage),
       portMappings: [{ containerPort: 8000 }],
       memoryReservationMiB: 50,
       cpu: 1,
+      logging: new AwsLogDriver({ streamPrefix: 'back-end' }),
     });
+
+    // Load Balancer
     new ApplicationLoadBalancedFargateService(this, "PTApp", {
       vpc: vpc,
       taskDefinition: taskDefinition,
@@ -65,6 +114,25 @@ export class EcsStack extends Stack {
       serviceName: 'PTApp',
       assignPublicIp: true,
       publicLoadBalancer: true,
-    })
+    });
+
+  // create DynamoDB table
+  const table = new Table(this, 'Table', {
+    partitionKey: { name: 'id', type: AttributeType.STRING },
+    tableName: 'MyTable',
+  });
+
+  backendContainer.addToExecutionPolicy(new PolicyStatement({
+    actions: [
+      'dynamodb:Query',
+      'dynamodb:Scan',
+      'dynamodb:GetItem',
+      'dynamodb:PutItem',
+      'dynamodb:UpdateItem',
+      'dynamodb:DeleteItem',
+    ],
+    resources: [table.tableArn],
+  }));
+
   }
 }
