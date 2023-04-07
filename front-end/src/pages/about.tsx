c71c025f50a6fdadb6c1eee5f9573be4f2d96c90
CalebReig <reigadacaleb@gmail.com>
2023-04-07 16:06:33 -0700

    fixed typo

--- front-end/src/pages/about.tsx
+++ front-end/src/pages/about.tsx
@@ -9,11 +9,11 @@ export default function About() {
       <div className={styles.content}>
         <h2 className={styles.subheading}>Who We Are</h2>
         <p className={styles.text}>
-          We are a team of dedicated personal trainers with a passion for helping our clients achieve their fitness goals. Our philosophy is simple: we believe that fitness should be accessible to everyone, regardless of their age, fitness level, or experience. That's why we offer a range of personalized training programs designed to meet your individual needs and goals.
+          We are a team of dedicated personal trainers with a passion for helping our clients achieve their fitness goals. Our philosophy is simple: we believe that fitness should be accessible to everyone, regardless of their age, fitness level, or experience. That&apos;s why we offer a range of personalized training programs designed to meet your individual needs and goals.
         </p>
         <h2 className={styles.subheading}>What We Do</h2>
         <p className={styles.text}>
-          Our personal training programs are tailored to your specific goals, whether you're looking to lose weight, build muscle, or improve your overall fitness. We work with you to create a customized workout plan that fits your schedule and lifestyle, and we provide the support and motivation you need to stay on track and reach your goals.
+          Our personal training programs are tailored to your specific goals, whether you&apos;re looking to lose weight, build muscle, or improve your overall fitness. We work with you to create a customized workout plan that fits your schedule and lifestyle, and we provide the support and motivation you need to stay on track and reach your goals.
         </p>
       </div>
     </div>
