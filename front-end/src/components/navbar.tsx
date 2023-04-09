7c3a5dc7b00eba9406d4941e7b063579c4fb92b1
CalebReig <reigadacaleb@gmail.com>
2023-04-08 17:04:27 -0700

    fixed webpage responsiveness

--- front-end/src/components/navbar.tsx
+++ front-end/src/components/navbar.tsx
@@ -1,17 +1,26 @@
 import Link from 'next/link';
 import styles from './styles/Navbar.module.css';
+import { useState } from 'react';
 
 export default function Navbar() {
+  const [showLinks, setShowLinks] = useState(false);
+
+  const toggleShowLinks = () => {
+    setShowLinks(!showLinks);
+  };
+
   return (
     <header>
       <nav className={styles.navbar}>
-        <div className={styles['navbar-logo']}>PT App</div>
-        <ul className={styles.navlinks}>
-          <li>
+        <div className={styles['navbar-logo']}>
             <Link href="/" className={styles.navbarLink}>
-              Home
+              PT App
             </Link>
-          </li>
+            <button className={styles['navbar-dropdown']} onClick={toggleShowLinks} >
+              <span className={styles['navbar-dropdown-icon']}>Menu</span>
+            </button>
+        </div>
+        <ul className={showLinks ? styles['navlinks-true']: styles['navlinks-false']}>
           <li>
             <Link href="/content/exercises" className={styles.navbarLink}>
               Exercises
@@ -25,6 +34,11 @@ export default function Navbar() {
           <li>
             <Link href="/pricing" className={styles.navbarLink}>
               Pricing
+            </Link>
+          </li>
+          <li>
+            <Link href="/products" className={styles.navbarLink}>
+              Products
             </Link>
           </li>
           <li>
